b7c75f76bd78960cf314aa2ee5eee1c69a041a2a
Gregory Danquoins <danquoinsgregory@gmail.com>
2024-01-08 21:27:07 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  constructor() {}
+  newUser = null;
+
+  username = new FormControl('aaaa');
+  email = new FormControl('jhg@hgjh.com');
+  password = new FormControl('');
+  addressLine = new FormControl('');
+  city = new FormControl();
+  cityCode = new FormControl();
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.newUser = {
+      username: this.username.value,
+      email: this.email.value,
+      password: this.password.value,
+      addressLine: this.addressLine.value,
+      city: this.city.value,
+      cityCode: this.cityCode.value,
+    };
+  }
+}
